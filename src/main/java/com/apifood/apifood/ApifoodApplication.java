b803d75e2bcd94a2295aaed24b6e7011ba83ac25
Matheus Valença <matheusniklaus@gmail.com>
2024-11-30 15:31:47 -0300

    mapeando primeiras entidades com JPA

---
+++ src/main/java/com/apifood/apifood/ApifoodApplication.java
@@ -0,0 +1,13 @@
+package com.apifood.apifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifoodApplication.class, args);
+	}
+
+}
